2b7a93a4bd5cf687b34cf15389c99cc1f1458219
Biki Gurung <bikigurung8@gmail.com>
2022-10-29 02:19:38 -0400

    changed terraform file

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -13,3 +13,27 @@
 #     external = 8000
 #   }
 # }
+
+
+module "ec2_instances" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "3.5.0"
+  count   = 1
+
+  name = "web_server01"
+
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
+  key_name               = "ubuntu_ec2"
+  monitoring             = true
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+  user_data              = "${file("deploy.sh")}"
+
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Name = "Webserver001"
+  }
+}
