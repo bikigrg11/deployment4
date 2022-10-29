e4227bab168ec2615a4f77d18704932f62ff5a93
Biki Gurung <bikigurung8@gmail.com>
2022-10-29 02:29:24 -0400

    changed the deploy script to add docker

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -2,12 +2,11 @@
 
 sudo apt update 
 sudo apt -y install git
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sleep 3
-sudo apt -y install python3-pip
-sleep 3
-git clone https://github.com/kura-labs-org/kuralabs_deployment_3.git
+
+git clone https://github.com/bikigrg11/deployment4.git
 sleep 2
-cd kuralabs_deployment_3/
-pip install -r requirements.txt
-pip install gunicorn
-python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon
+cd deployment4/
+docker-compose up --build
+
