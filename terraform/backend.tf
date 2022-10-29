cd40990913c01dd6f6020a01dbefd6890677a382
Biki Gurung <bikigurung8@gmail.com>
2022-10-29 02:05:45 -0400

    added secret AWS keys

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -20,9 +20,9 @@ resource "aws_s3_bucket" "terraform_state" {
 
 resource "aws_s3_bucket_versioning" "enabled" {
   bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  # versioning_configuration {
+  #   status = "Enabled"
+  # }
 }
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
